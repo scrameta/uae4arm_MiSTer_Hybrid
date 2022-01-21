5e565db9f529f6ee33cf857aa2310f00e572d404
scrameta <scrameta@gmail.com>
2022-01-21 20:08:57 +0100

    Remove a load of definitely not cpu emulation code

--- src/include/calc.h
+++
@@ -1,8 +0,0 @@
-#ifndef UAE_CALC_H
-#define UAE_CALC_H
-
-#include "uae/types.h"
-
-extern bool calc(const TCHAR *input, double *outval);
-
-#endif /* UAE_CALC_H */
--- src/include/uaeresource.h
+++
@@ -1,9 +0,0 @@
-#ifndef UAE_UAERESOURCE_H
-#define UAE_UAERESOURCE_H
-
-#include "uae/types.h"
-
-uaecptr uaeres_startup(TrapContext *ctx, uaecptr resaddr);
-void uaeres_install(void);
-
-#endif /* UAE_UAERESOURCE_H */
